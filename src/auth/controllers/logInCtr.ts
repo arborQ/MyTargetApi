4843a6d98ee34b4202e025d66f0b7bae793b91d9
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2015-10-28 13:31:18 +0100

    refactor

--- src/auth/controllers/logInCtr.ts
+++ src/auth/controllers/logInCtr.ts
@@ -4,25 +4,12 @@ class logInCtr {
   constructor($http : angular.IHttpService, $rootScope : any, authService : application.auth.IAuthService, $state : ng.ui.IStateService) {
     this.save = (model: any, form: angular.IFormController, $event : ng.IAngularEvent) => {
       $event.preventDefault();
-      $event.stopPropagation();
-
       if (form.$valid) {
-        $rootScope.$loading = true;
         $http.post('/authorization/login', model)
         .success((response : any) => {
           authService.SetToken(response.token);
           $state.go('users');
-        })
-        .finally(() => $rootScope.$loading = false);
-        // .save(model)
-        // .$promise.then((res) => {
-        //   if(res.token){
-        //     authService.SetToken(res.token);
-        //     $state.go('users');
-        //   }
-        // }).finally(() =>{
-        //   $rootScope.$loading = false;
-        // });
+        });
       }
     }
   }
